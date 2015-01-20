cd93fbc3b1162541727eb76a91b1cf31f0b1987b
kyo <py4762013@126.com>
2015-01-20 06:45:12 +0000

    session-mongodb

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'RabbitLALALA!'});
+});
+
+router.get('/login', function(req, res){
+    res.render('login', { title: "用户登陆" });
+});
+
+router.post('/login', function(req, res){
+    var user = {
+        username: 'admin',
+        password: 'admin'
+    }
+
+    if(req.body.username === user.username && req.body.password === user.password){
+        res.redirect('/home');
+    }
+
+    res.redirect('/login');
+});
+
+router.get('/logout', function(req, res){
+    res.redirect('/');
+});
+
+router.get('/home', function(req, res){
+    var user = {
+        username: 'admin',
+        password: 'admin'
+    }
+    res.render('home', { title: 'Home', user: user});
 });
 
 module.exports = router;
